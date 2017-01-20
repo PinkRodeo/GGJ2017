1e7097fba5858d0fdebe10caff2dc37b6d6fbb53
Robin Zaagsma <zaagsma@robin.local>
2017-01-21 00:42:41 +0100

    +added combo system to midi controller +added static game controller +improved audio ducking of static noise

---
+++ GGJ2017_Project/Assets/Game.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+	public static MidiController midiController;
+	public Transform tuner;
+
+	void Start(){
+		//midiController = gameObject.AddComponent<MidiController> ();
+		midiController = new MidiController();
+		midiController.tuner = tuner;
+	
+		string[] combo = {"e2","e2","e2","c2","e2","g2"};
+		midiController.AddComboListener(combo, ComboTest);
+	}
+
+
+	void ComboTest(){
+		Debug.Log("test combo entered");
+	}
+}
--- GGJ2017_Project/Assets/MidiController.cs
+++ GGJ2017_Project/Assets/MidiController.cs
@@ -2,28 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MidiController : MonoBehaviour {
+public delegate void ComboDelegate();
 
-	public Transform go;
+public class ComboListener {
+	public string[] combo;
+	public ComboDelegate callback;
+}
+
+public class MidiController {
+
+	List<string> lastNotes;
+	List<ComboListener> comboListeners;
+
+	public Transform tuner;
+
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	public MidiController () {
 		MidiJack.MidiMaster.knobDelegate = OnKnob;
 		MidiJack.MidiMaster.noteOnDelegate = OnNoteOn;
+
+		lastNotes = new List<string> ();
+		comboListeners = new List<ComboListener> ();
+	}
+		
+
+	void FlushCombo(){
+		lastNotes = new List<string> ();
+	}
+
+	public void AddComboListener (string[] combo, ComboDelegate callback) {
+		ComboListener listener = new ComboListener ();
+		listener.combo = combo;
+		listener.callback = callback;
+
+		comboListeners.Add (listener);
 	}
 
 	void OnNoteOn(MidiJack.MidiChannel channel, int noteNumber, float velocity){
-		Debug.Log (noteNumber.ToString() +' '+ velocity.ToString());
+		string name = NoteToString (noteNumber);
+		lastNotes.Add (name);
+
+		//TODO check for combos;
+		int ii;
+		int jj;
+		for (ii = comboListeners.Count - 1; ii >= 0; ii--) {
+			bool isComplete = true;
+			ComboListener listener = comboListeners [ii];
+			for (jj = 0; jj < listener.combo.Length; jj++) {
+				int k = listener.combo.Length -1 - jj;
+				int p = lastNotes.Count - 1 - jj;
+
+				if (lastNotes [p] != listener.combo [k]) {
+					isComplete = false;
+					break;
+				}
+			}
+			if (isComplete) {
+				listener.callback ();
+			}
+		}
 	}
 
 	void OnKnob(MidiJack.MidiChannel channel, int knobNumber, float knobValue){
 		knobValue = Mathf.Round (knobValue * 127);
-		Debug.Log (knobNumber.ToString() +' '+ knobValue.ToString());
-		var pos = go.localPosition;
+		var pos = tuner.localPosition;
 
 		switch (knobNumber) {
 		case 74:
@@ -36,6 +78,29 @@ public class MidiController : MonoBehaviour {
 			pos.z = knobValue;
 			break;
 		}
-		go.localPosition = pos;
+		tuner.localPosition = pos;
+	}
+
+	string[] letters = {"c","c#","d","d#","e","f","f#","g","g#","a","a#","b"};
+
+	string NoteToString(int note){
+		note -= 48;
+
+		string letter = letters[note%12];
+		string number = Mathf.Ceil((note + 1.0f) / 12.0f).ToString();
+
+		return letter + number;
+	}
+
+	int StringToNote(string note){
+		int ii;
+		string letter = note.Substring (0, note.Length - 1);
+		int number = int.Parse(note.Substring (note.Length - 1));
+		for (ii = 0; ii < 12; ii++) {
+			if (letters [ii] == letter) {
+				break;
+			}
+		}
+		return ii + (number-1)*12 + 48;
 	}
 }
