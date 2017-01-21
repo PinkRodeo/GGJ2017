c0bbee2095b78f1f88a45ff5621f4f454b9aba06
Robin Zaagsma <zaagsma@robin.local>
2017-01-21 11:32:18 +0100

    *Set volume of static noise

--- GGJ2017_Project/Assets/Scripts/Game.cs
+++ GGJ2017_Project/Assets/Scripts/Game.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 namespace HAM
 {
 	public class Game : MonoBehaviour
 	{
+		public AudioSource staticChannel;
 
 		public static MidiController midiController;
 		public static SpeechController speechController;
+		private static AudioSource sStaticChannel;
 
 		public GUIText guiText;
 
@@ -20,12 +23,13 @@ namespace HAM
 			speechController = new SpeechController ();
 			speechController.guiText = guiText;
 
-
 			//TESTS
 			string[] combo = { "e2", "e2", "e2", "c2", "e2", "g2" };
 			midiController.AddComboListener(combo, ComboTest);
 
 			speechController.Say("hello world");
+
+			sStaticChannel = staticChannel;
 		}
 
 		void Update(){
@@ -36,6 +40,10 @@ namespace HAM
 		void ComboTest ()
 		{
 			Debug.Log("test combo entered");
+		}
+
+		public static void SetStaticVolume (float volume){
+			sStaticChannel.volume = volume;
 		}
 	}
 
